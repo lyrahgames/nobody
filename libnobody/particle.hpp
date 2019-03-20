a8f191668e55ee4444c8d775a581a3e87400d042
lyrahgames <markuspawellek@gmail.com>
2019-03-20 23:08:40 +0100

    Start with library for application

---
+++ libnobody/particle.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef NOBODY_LIBNOBODY_PARTICLE_HPP_
+#define NOBODY_LIBNOBODY_PARTICLE_HPP_
+
+#include <type_traits>
+#include <utility>
+
+namespace nobody {
+
+template <typename Vector>
+class particle {
+ public:
+  using vector_type = Vector;
+  using real_type = std::decay_t<decltype(Vector{}[0])>;
+
+  particle() = default;
+  particle(const particle&) = default;
+  particle& operator=(const particle&) = default;
+  particle(particle&&) = default;
+  particle& operator=(particle&&) = default;
+  ~particle() = default;
+
+  const auto& position() const noexcept { return position_; }
+  auto& position() noexcept { return position_; }
+  particle& position(const vector_type& p) & noexcept {
+    position_ = p;
+    return *this;
+  }
+  particle position(const vector_type& p) && noexcept {
+    position_ = p;
+    return std::move(*this);
+  }
+
+  const auto& velocity() const noexcept { return velocity_; }
+  auto& velocity() noexcept { return velocity_; }
+  particle& velocity(const vector_type& v) & noexcept {
+    velocity_ = v;
+    return *this;
+  }
+  particle velocity(const vector_type& v) && noexcept {
+    velocity_ = v;
+    return std::move(*this);
+  }
+
+  auto mass() const noexcept { return mass_; }
+  particle& mass(real_type m) & {
+    if (m <= 0)
+      throw std::invalid_argument(
+          "Mass of a particle cannot be lower or equal to zero!");
+    mass_ = m;
+    return *this;
+  }
+  particle mass(real_type m) && {
+    if (m <= 0)
+      throw std::invalid_argument(
+          "Mass of a particle cannot be lower or equal to zero!");
+    mass_ = m;
+    return std::move(*this);
+  }
+
+ private:
+  vector_type position_{};
+  vector_type velocity_{};
+  real_type mass_{1};
+};
+
+}  // namespace nobody
+
+#endif  // NOBODY_LIBNOBODY_PARTICLE_HPP_
---
+++ libnobody/particle.test.cpp
@@ -0,0 +1,52 @@
+#include <doctest/doctest.h>
+
+#include <Eigen/Dense>
+#include <libnobody/particle.hpp>
+#include <type_traits>
+
+TEST_CASE("The particle") {
+  using vector_type = Eigen::Vector3f;
+  using particle = nobody::particle<vector_type>;
+
+  static_assert(std::is_same_v<particle::real_type, float>);
+  static_assert(std::is_same_v<particle::real_type, vector_type::value_type>);
+
+  SUBCASE("Setters and getters") {
+    particle p{};
+    CHECK(p.position() == particle::vector_type{0, 0, 0});
+    CHECK(p.velocity() == particle::vector_type{0, 0, 0});
+    CHECK(p.mass() == 1);
+
+    p.position() = {1, 2, 3};
+    CHECK(p.position() == particle::vector_type{1, 2, 3});
+    CHECK(p.velocity() == particle::vector_type{0, 0, 0});
+    CHECK(p.mass() == 1);
+
+    p.velocity() = {4, 5, 6};
+    CHECK(p.position() == particle::vector_type{1, 2, 3});
+    CHECK(p.velocity() == particle::vector_type{4, 5, 6});
+    CHECK(p.mass() == 1);
+
+    CHECK_THROWS_AS(p.mass(0), std::invalid_argument);
+    CHECK_THROWS_AS(p.mass(-1), std::invalid_argument);
+    p.mass(2);
+    CHECK(p.position() == particle::vector_type{1, 2, 3});
+    CHECK(p.velocity() == particle::vector_type{4, 5, 6});
+    CHECK(p.mass() == 2);
+  }
+
+  SUBCASE("lvalue setters") {
+    particle p{};
+    p.position({1, 1, 1}).velocity({2, 2, 2}).mass(3);
+    CHECK(p.position() == particle::vector_type{1, 1, 1});
+    CHECK(p.velocity() == particle::vector_type{2, 2, 2});
+    CHECK(p.mass() == 3);
+  }
+
+  SUBCASE("rvalue setters") {
+    const auto p = particle{}.position({5, 5, 5}).velocity({6, 6, 6}).mass(2);
+    CHECK(p.position() == particle::vector_type{5, 5, 5});
+    CHECK(p.velocity() == particle::vector_type{6, 6, 6});
+    CHECK(p.mass() == 2);
+  }
+}
